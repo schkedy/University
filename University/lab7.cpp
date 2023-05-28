8eb535fee0c6dca5c5bf3d13788e68c5b2252a9a
len <schkedy@gmail.com>
2023-05-28 20:41:11 +0700

    add 7 TASK POLIZ CREATOR

--- University/lab7.cpp
+++ University/lab7.cpp
@@ -2,48 +2,146 @@
 #include "/home/len/CLionProjects/mylib/mylib.cpp"
 #include <cstdio>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
-struct Usel{
+struct Usel {
     int data; // данные
     Usel *next; //ссылка на следующий узел
 };
-Usel* push(Usel *head, int dataNew){
+
+struct Stack {
+    Usel *head;
+
+    Stack() { head = 0; }; // Конструктор
+    ~Stack() { clear(); }; // Деструктор
+    void push(int k);
+
+    int pop();
+
+    int top();
+
+    bool empty();
+
+    void clear();
+
+};
+
+Usel *push(Usel *head, int dataNew) {
     Usel *p = new Usel; //Создаем новый узел
-    p->data=dataNew; // Пишем в него данные
+    p->data = dataNew; // Пишем в него данные
     p->next = head; // В next  пишем ссылку предыдущей верхушки
     head = p; // Меняем верхушку
     return head; // Возвращаем верхушку
 }
-Usel* pop(Usel *head, int *dataGet){
+
+Usel *pop(Usel *head, int *dataGet) {
     *dataGet = head->data; // Сохраняем данные из бывшей верхушки
     Usel *p = head;  // делаем указатель чтобы изменить верхушку
-    head=head->next; // меняем верхушку
+    head = head->next; // меняем верхушку
     delete p;        // удаляем бывшую верхушку
     return head;
 }
-int top (Usel *head){
+
+int top(Usel *head) {
     return head->data;
 }
-bool empty (Usel *head){
+
+bool empty(Usel *head) {
     if (head == NULL) return true;
     return false;
 }
-void clear (Usel *head){
+
+void clear(Usel *head) {
     int k;
-    while (head) head=pop(head, NULL); // Не нужно сохранять данные
+    while (head) head = pop(head, NULL); // Не нужно сохранять данные
 }
-void output (Usel *head){
+
+void output(Usel *head) {
     Usel *p;
-    for(p=head; p!=0; p=p->next) // Начинаем с вершины и идем пока не дойдем до конца
-        cout<<p->data<<' ';
+    for (p = head; p != 0; p = p->next) // Начинаем с вершины и идем пока не дойдем до конца
+        cout << p->data << ' ';
 }
-int main(){
-Usel *head = NULL;
-int k,i;
-    head = push(head,5);
-    output(head);
+
+void Stack::clear() {
+    int k;
+    while (head) k = pop();
+}
+
+void Stack::push(int k) {
+    Usel *p = new Usel;
+    p->data = k;
+    p->next = head;
+    head = p;
+}
+
+int Stack::pop() {
+    int k = head->data;
+    Usel *p = head;
+    head = head->next;
+    delete p;
+    return k;
+}
+
+int Stack::top() {
+    return head->data;
+}
+
+bool Stack::empty() {
+    if (head == 0) return true;
+    return false;
+}
+
+int main() {
+    char s[80], p[80], ch;
+    Stack Znak, Prior;
+    int i, j;
+    puts("введите формулу:");
+
+    cin.getline(s, 79);
+
+    for (i = j = 0; s[i]; i++)
+        if (digit(s[i])) { // если цифра, то ее в ПОЛИЗ
+            p[j++] = s[i];
+        } else if (Znak.empty() || s[i] == '(') {
+            Znak.push(s[i]); // операцию в стек операций
+            Prior.push(prioritet(s[i]));
+        } else if (s[i] == ')') {// если текущий символ ')'
+            while (!Znak.empty() && Znak.top() != '(') {
+                p[j++] = Znak.pop();
+                Prior.pop();
+            }
+            if(Znak.empty()) {
+                perror("Не нашли (");
+                exit(-1);
+            }
+            Znak.pop();
+            Prior.pop();
+        } else if (Prior.top() < prioritet(s[i])) { // приоритет операции в стеке < приоритета текущей
+            Znak.push(s[i]); // помещаем операцию в стек операций
+            Prior.push(prioritet(s[i]));
+        } else {
+            while (!Znak.empty() && Prior.top() >= prioritet(s[i])) {
+                p[j++] = Znak.pop();
+                Prior.pop();
+            }
+            Znak.push(s[i]); //после этого кладем в стек текущую операцию,
+            Prior.push(prioritet(s[i]));
+        }
+    //if (strlen(s))
+    while (!Znak.empty()) {
+        if (Znak.top() == '(') {
+            perror("Отсутствует ')'");
+            exit(-1);
+        }
+        p[j++] = Znak.pop();
+        Prior.pop();
+    }
+    p[j] = '\0';
+    cout << " ПОЛИЗ: " << p << endl;
+
 }
--- mylib/mylib.cpp
+++ mylib/mylib.cpp
@@ -1,14 +1,16 @@
 #include "mylib.h"
 #include <cstdio>
 #include <iostream>
+
 #define UI unsigned int
+
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
 
 //Сдвигает маску на k компоненту если и возвращает побитовый и
-bool definebit(UI value, int k){ //Сдвигает маску на k компоненту и возвращает побитовый и
-    UI mask =1;
+bool definebit(UI value, int k) { //Сдвигает маску на k компоненту и возвращает побитовый и
+    UI mask = 1;
     mask = mask << k; // mask <<=k
     if ((value & mask) == 0)
         return 0;
@@ -16,15 +18,15 @@ bool definebit(UI value, int k){ //Сдвигает маску на k компо
         return 1;
 }
 //УСТАНАВЛИВАЕТ K КОМПОНЕТУ БУЛЕВА ВЕКТОРА (СТАВИТ 1)
-UI setUp1(UI x, int k){
-    UI mask=1;
+UI setUp1(UI x, int k) {
+    UI mask = 1;
     mask <<= k;
-    x|=mask;
+    x |= mask;
     return x;
 }
 //СБРАСЫВАЕТ K КОМПОНЕНТУ БУЛЕВА ВЕКТОРА (СТАВИТ 0)
-UI setUp0(UI x, int k){
-    UI mask=1;
+UI setUp0(UI x, int k) {
+    UI mask = 1;
     mask <<= k; // 000001000
     mask = ~mask; // 111110111
     x &= mask; // 111001111 & 111110111 = 111000111
@@ -47,83 +49,91 @@ UI invert(UI x, int k) {
 }
 
 //НАХОДИТ ВЕС (КОЛИЧЕСТВО ЕДИНИЦ БУЛЕВА ВЕКТОРА)
-int weight (UI x, int n)
-{
+int weight(UI x, int n) {
     int w, i;
-    UI mask=1;
-    for (w=0, i=0; i<n; i++, mask<<=1)// СДВИГАЕТ ЕДИНИЦУ НА КАЖДУЮ К ПОЗИЦИЮ И ПОБИТОВО И
-        if (x&mask) {
+    UI mask = 1;
+    for (w = 0, i = 0; i < n; i++, mask <<= 1)// СДВИГАЕТ ЕДИНИЦУ НА КАЖДУЮ К ПОЗИЦИЮ И ПОБИТОВО И
+        if (x & mask) {
             w++;
         }
     return w;
 }
 //ВВОД БУЛЕВОГО ВЕКТОРА ПО ЧАРУ
-UI input2BV (char s[], int n){
-    UI x=0;
+UI input2BV(char s[], int n) {
+    UI x = 0;
     int i;
-    for(i=0; i<n; i++){
-        x<<=1;
-        if (s[i]=='1')
+    for (i = 0; i < n; i++) {
+        x <<= 1;
+        if (s[i] == '1')
             x++;  // CHAR[10100000]: 00000000 ->00000001 -> 00000010-> 000000101 ->...-> 10100000
     }
     return x;
 }
+
 //ВЫВОД БУЛЕВОГО ВЕКТОРА
-void outputBV(UI x, int n){
-    UI mask = 1; int i;
-    mask<<=n-1;
-    for(i=0; i < n; i++, mask>>=1)
-        if (x&mask) putchar('1');
-    else putchar('0');
+void outputBV(UI x, int n) {
+    UI mask = 1;
+    int i;
+    mask <<= n - 1;
+    for (i = 0; i < n; i++, mask >>= 1)
+        if (x & mask) putchar('1');
+        else putchar('0');
 }
+
 //ВВОД БУЛЕВОЙ МАТРИЦЫ
-void inputBM (UI *bm, int m, int n){
+void inputBM(UI *bm, int m, int n) {
     int i;
     char s[33];
-    for (i=0; i<m; i++){
-        fgets(s,33,stdin); //считывает строку n-1 символа из входного потока stdin и пишет ее char[]
-        *(bm+i)=input2BV(s, n);
+    for (i = 0; i < m; i++) {
+        fgets(s, 33, stdin); //считывает строку n-1 символа из входного потока stdin и пишет ее char[]
+        *(bm + i) = input2BV(s, n);
     }
 }
+
 //ВЫВОД БУЛЕВОЙ МАТРИЦЫ
-void outputBM (UI *bm, int m, int n){
+void outputBM(UI *bm, int m, int n) {
     int i;
-    for (i=0; i<m; i++){
-        outputBV(*(bm+i), n);
+    for (i = 0; i < m; i++) {
+        outputBV(*(bm + i), n);
         puts(" ");
     }
 }
+
 //ВЕС БУЛЕВОГО СТОЛБЦА
 int weightColumn(UI *bm, int m, int n, int k) {
     int w, i;
-    UI mask = 1 << k ;               //ставит единицу в k позицию
+    UI mask = 1 << k;               //ставит единицу в k позицию
     for (w = 0, i = 0; i < m; i++)  //проходит по строкам
-        if (*(bm+i) & mask) w++;
+        if (*(bm + i) & mask) w++;
     return w;
 }
-int countDifferentPos(UI a,UI b,int n){
-    UI c = a^b;
-    return weight(c,n);
+
+int countDifferentPos(UI a, UI b, int n) {
+    UI c = a ^ b;
+    return weight(c, n);
 }
-int countDifferentPosAndPos(UI a,UI b,int n){
-    UI c = a^b;
-    return weight(c,n);
+
+int countDifferentPosAndPos(UI a, UI b, int n) {
+    UI c = a ^ b;
+    return weight(c, n);
 }
-void changeColumn(UI *bm,int m, int columnA, int columnB ) {
-    bool changeA,changeB;
+
+void changeColumn(UI *bm, int m, int columnA, int columnB) {
+    bool changeA, changeB;
     if (columnB != columnA)
-        for (int i = 0;i<m;i++){
-            changeB = definebit(*(bm+i),columnA); //присваевам в changeB бит в столбце А
-            changeA = definebit(*(bm+i),columnB);//присваевам в changeB бит в столбце Б
+        for (int i = 0; i < m; i++) {
+            changeB = definebit(*(bm + i), columnA); //присваевам в changeB бит в столбце А
+            changeA = definebit(*(bm + i), columnB);//присваевам в changeB бит в столбце Б
             if (changeB != changeA) {
                 *(bm + i) = setUp(*(bm + i), columnA, changeA);
                 *(bm + i) = setUp(*(bm + i), columnB, changeB);
             }
-            }
+        }
 }
-int indexMaxWeightColumnBM(UI *bm,int m,int n){
-    int maxWeight = 0,index,weight;
-    for(int i = 0;i<=n;i++) {
+
+int indexMaxWeightColumnBM(UI *bm, int m, int n) {
+    int maxWeight = 0, index, weight;
+    for (int i = 0; i <= n; i++) {
         weight = weightColumn(bm, m, n, i);
         if (weight > maxWeight) {
             index = i;
@@ -132,74 +142,83 @@ int indexMaxWeightColumnBM(UI *bm,int m,int n){
     }
     return index;
 }
+
 //ВОЗВРАЩАЕТ РАНДОМНОЕ ЦЕЛОЕ ЧИСЛО
-int getRandomNumber(int minBorder,int maxBorder){
+int getRandomNumber(int minBorder, int maxBorder) {
     srand(time(0));
-    int number = minBorder + rand() % (maxBorder - minBorder -1);
-    return  number;
+    int number = minBorder + rand() % (maxBorder - minBorder - 1);
+    return number;
 }
 
-int getRandomNumber(int minBorder){
+int getRandomNumber(int minBorder) {
 
     int number = minBorder + rand() % (INT32_MAX - minBorder - 1);
-    return  number;
+    return number;
 }
-void checkFile(FILE *f){
-    if (f==NULL) {
+
+void checkFile(FILE *f) {
+    if (f == NULL) {
         perror("error opening file");
         exit(132);
     }
 }
-void writeRandFileMin(FILE *f, int min,int size){
+
+void writeRandFileMin(FILE *f, int min, int size) {
     int number;
-    for (int i = min;i<min+size;i++) {
-        number = getRandomNumber(i,100);
+    for (int i = min; i < min + size; i++) {
+        number = getRandomNumber(i, 100);
         fprintf(f, "%d ", number);
     }
 }
-void writeRandFileMinMax(FILE *f, int min,int max,int size){
+
+void writeRandFileMinMax(FILE *f, int min, int max, int size) {
     int number;
-    for (int i = 0;i<size;i++) {
-        number = getRandomNumber(min,100);
+    for (int i = 0; i < size; i++) {
+        number = getRandomNumber(min, 100);
         fprintf(f, "%d ", number);
     }
 }
-void fRead(char name[]){
+
+void fRead(char name[]) {
     int a;
     FILE *f;
-    f = fopen(name,"r");
-    while(fscanf(f,"%d",&a)!=EOF)
-        printf("%d ",a);
+    f = fopen(name, "r");
+    while (fscanf(f, "%d", &a) != EOF)
+        printf("%d ", a);
     fclose(f);
     printf("\n");
 }
-void fRead(char name[],char text[]){
+
+void fRead(char name[], char text[]) {
     int a;
     FILE *f;
-    f = fopen(name,"r");
-    while(fscanf(f,"%d",&a)!=EOF)
-        printf("%d ",a);
+    f = fopen(name, "r");
+    while (fscanf(f, "%d", &a) != EOF)
+        printf("%d ", a);
     fclose(f);
     puts(text);
     printf("\n");
 }
-void printArray(int *array, int size){
-    for(int i = 0;i < size;i++)
+
+void printArray(int *array, int size) {
+    for (int i = 0; i < size; i++)
         printf("%d ", array[i]);
     printf("\n");
 }
-void printTime(){
+
+void printTime() {
     clock_t start = clock();
     sleep(0);
     clock_t end = clock();
-    double seconds  = (double)(end -start) / CLOCKS_PER_SEC;
-    printf("the time :%f seconds \n",seconds);
+    double seconds = (double) (end - start) / CLOCKS_PER_SEC;
+    printf("the time :%f seconds \n", seconds);
 }
-bool sortBubble(int *array,int size) {
+
+bool sortBubble(int *array, int size) {
     clock_t start = clock();
     int middle, lastChange;
     lastChange = size - 1;
-    for (int k = size -1; (k > 0); k--) {
+    for (int k = size - 1; (k > 0); k--) {
         //lastChange = 0;
         for (int i = 0; i < k; i++)
             if (array[i] > array[i + 1]) {
@@ -212,21 +231,22 @@ bool sortBubble(int *array,int size) {
         //printArray(array, size);
     }
     clock_t end = clock();
-    double seconds  = (double)(end -start) / CLOCKS_PER_SEC;
+    double seconds = (double) (end - start) / CLOCKS_PER_SEC;
     puts("Bubble ->");
-    printf("the time :%f seconds \n",seconds);
+    printf("the time :%f seconds \n", seconds);
     if (size >= 30)
         return 0;
     else return 1;
 
 
 }
-bool sortShaker(int *array,int size){
+
+bool sortShaker(int *array, int size) {
     clock_t start = clock();
     int middle;
     int lastChange;
     int firstchange = 0;
-    for (int k = size -1,b = 0; (k > 0)&(b<size-1); k--,b++) {
+    for (int k = size - 1, b = 0; (k > 0) & (b < size - 1); k--, b++) {
         //lastChange = 0;
         firstchange = 0;
         for (int i = b; i < k; i++)     //i = b
@@ -238,10 +258,10 @@ bool sortShaker(int *array,int size){
             }
         //k = lastChange;
         //printArray(array, size);
-        for (int i = --k;i > b ;i--) //i > 0
-            if(array[i] < array[i-1]){
-                middle = array[i-1];
-                array[i-1] = array[i];
+        for (int i = --k; i > b; i--) //i > 0
+            if (array[i] < array[i - 1]) {
+                middle = array[i - 1];
+                array[i - 1] = array[i];
                 array[i] = middle;
                 //firstchange = i;
             }
@@ -249,124 +269,155 @@ bool sortShaker(int *array,int size){
         //printArray(array, size);
     }
     clock_t end = clock();
-    double seconds  = (double)(end -start) / CLOCKS_PER_SEC;
+    double seconds = (double) (end - start) / CLOCKS_PER_SEC;
     puts("Shaker ->");
-    printf("the time :%f seconds \n",seconds);
+    printf("the time :%f seconds \n", seconds);
     if (size >= 30)
         return 0;
     else return 1;
 
 }
-void randomArray (int *array,int size,int minBorder,int maxBorder,int *array1){
+
+void randomArray(int *array, int size, int minBorder, int maxBorder, int *array1) {
     srand(time(0));
-    for(int i = 0;i<size;i++){
-        *(array+i) = minBorder + rand() % (maxBorder - minBorder -1);
-        *(array1+i) = *(array+i);
+    for (int i = 0; i < size; i++) {
+        *(array + i) = minBorder + rand() % (maxBorder - minBorder - 1);
+        *(array1 + i) = *(array + i);
     }
 }
-void randomArray (int *array,int size,int minBorder,int maxBorder,int *array1,int *array2){
+
+void randomArray(int *array, int size, int minBorder, int maxBorder, int *array1, int *array2) {
     srand(time(0));
-    for(int i = 0;i<size;i++){
-        *(array+i) = minBorder + rand() % (maxBorder - minBorder -1);
-        *(array1+i) = *(array+i);
-        *(array2+i) = *(array+i);
+    for (int i = 0; i < size; i++) {
+        *(array + i) = minBorder + rand() % (maxBorder - minBorder - 1);
+        *(array1 + i) = *(array + i);
+        *(array2 + i) = *(array + i);
     }
 }
-void randomSortArray(int *array,int size,int minBorder,int maxBorder){
-    int step = (maxBorder - minBorder)/size;
+
+void randomSortArray(int *array, int size, int minBorder, int maxBorder) {
+    int step = (maxBorder - minBorder) / size;
     maxBorder = minBorder + step;
-    for(int i = 0;i<size;i++){
+    for (int i = 0; i < size; i++) {
         maxBorder = minBorder + step;
-        array[i] = getRandomNumber(minBorder,maxBorder);
+        array[i] = getRandomNumber(minBorder, maxBorder);
         minBorder = maxBorder;
     }
 }
-int searchBinary(int *array,int size,int a){
-    int L = 0,R = size-1;
-    while(L<R){
-        int move = (L+R)/2;
-        if(a == array[move])
+
+int searchBinary(int *array, int size, int a) {
+    int L = 0, R = size - 1;
+    while (L < R) {
+        int move = (L + R) / 2;
+        if (a == array[move])
             return move;
-        if (array[move]<a){
-            L = move +1;
+        if (array[move] < a) {
+            L = move + 1;
         }
         if (a < array[move])
-            R =move -1;
+            R = move - 1;
     }
     puts("Элемент не найден");
     return -1;
 }
-int getStrSize(char *s){
-    int count = 0,i = 0;
-    while(true){
-        if(s[i++] == 0)
+
+int getStrSize(char *s) {
+    int count = 0, i = 0;
+    while (true) {
+        if (s[i++] == 0)
             return count;
         else count++;
     }
 }
-void printPart(char *s,int start,int end){
+
+void printPart(char *s, int start, int end) {
     int i = start;
-    while(i<end){
-        printf("%c",s[i++]);
+    while (i < end) {
+        printf("%c", s[i++]);
     }
     printf("\n");
 }
-int searchBM(char *s , char *p){
-    int TAB[256],sizeS,sizeP,count=0,cc=0;
-    sizeS = strlen(s)-1;
-    sizeP = strlen(p)-1;
-    for(int i =0; i<256; i++) //
-        TAB[i]= sizeP;
-    for(int i =0; i<sizeP-1; i++)
-        TAB[p[i]]=sizeP-1-i;   // Присваивает ASCII таблице степень удаленности от конца подстроки
 
-    for(int i = sizeP - 1;i<sizeS;i+=count) {
-        cc= 0;
+int searchBM(char *s, char *p) {
+    int TAB[256], sizeS, sizeP, count = 0, cc = 0;
+    sizeS = strlen(s) - 1;
+    sizeP = strlen(p) - 1;
+    for (int i = 0; i < 256; i++) //
+        TAB[i] = sizeP;
+    for (int i = 0; i < sizeP - 1; i++)
+        TAB[p[i]] = sizeP - 1 - i;   // Присваивает ASCII таблице степень удаленности от конца подстроки
+
+    for (int i = sizeP - 1; i < sizeS; i += count) {
+        cc = 0;
         for (int j = sizeP - 1, l = i; j >= 0; j--, l--) {
-            printf("%c %c\n",s[l],p[j]);
-           getchar();
+            printf("%c %c\n", s[l], p[j]);
+            getchar();
             if (s[l] != p[j]) {
                 count = TAB[s[i]];
                 j = 0;
-            }
-            else if(j == 0) return l;
+            } else if (j == 0) return l;
         }
         //printPart(s,i-sizeP+1,i+1);
     }
     return -1;
 }
-int searchBinaryIntegrate(int *array,int size,int a){
-    int L = 0,R = size,move;
-    while(L<=R){
-        move = (L+R)/2;
-        if(a == array[move])
+
+int searchBinaryIntegrate(int *array, int size, int a) {
+    int L = 0, R = size, move;
+    while (L <= R) {
+        move = (L + R) / 2;
+        if (a == array[move])
             return move;
-        if (a> array[move]){
-            L = move +1;
+        if (a > array[move]) {
+            L = move + 1;
         }
         if (a < array[move])
-            R =move -1;
+            R = move - 1;
     }
-    return R+1;
+    return R + 1;
 }
-bool sortInsertBinary(int *array, int size){
+
+bool sortInsertBinary(int *array, int size) {
     clock_t start = clock();
-    int middle,move;
-    for(int i = 1;i<size;i++)
-        if (array[i]<array[i-1]){
-        move = searchBinaryIntegrate(array, i, array[i]);//Возвращает индекс вставки числа
-        int backStep = i;
-        while (move != backStep) {
-            middle = array[backStep-1];
-            array[backStep-1] = array[backStep];
-            array[backStep--] = middle;
+    int middle, move;
+    for (int i = 1; i < size; i++)
+        if (array[i] < array[i - 1]) {
+            move = searchBinaryIntegrate(array, i, array[i]);//Возвращает индекс вставки числа
+            int backStep = i;
+            while (move != backStep) {
+                middle = array[backStep - 1];
+                array[backStep - 1] = array[backStep];
+                array[backStep--] = middle;
+            }
         }
-    }
     clock_t end = clock();
-    double seconds  = (double)(end -start) / CLOCKS_PER_SEC;
+    double seconds = (double) (end - start) / CLOCKS_PER_SEC;
     puts("InsertBinary ->");
-    printf("the time :%f seconds \n",seconds);
+    printf("the time :%f seconds \n", seconds);
     if (size >= 30)
         return 0;
     else return 1;
+}
+
+bool digit(char ch) {
+    if ('0' <= ch && ch <= '9') return true;
+    return false;
+}
+
+int prioritet(char s) {
+    switch (s) {
+        case '(':
+            return 0;
+        case ')':
+            return 1;
+        case '+':
+            return 2;
+        case '-':
+            return 2;
+        case '*':
+            return 3;
+        case '/':
+            return 3;
+    }
+    return -1;
 }
